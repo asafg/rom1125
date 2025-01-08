7a202bce7dadd39ef87b41ab3cd993ecb66d8d23
Asaf Gery <asaf.gery@gmail.com>
2025-01-09 00:10:52 +0200

    Synced between website and github. Updated prices. Commented out inactive link - "In the area"

---
+++ rom1125/WebContent/js/mobile_redirection.js
@@ -0,0 +1,6 @@
+if ((navigator.userAgent.match(/(iPhone|iPod|BlackBerry|Android|webOS|Windows CE|IEMobile|Opera Mini|Opera Mobi|HTC|LG-|LGE|SAMSUNG|Samsung|SEC-SGH|Symbian|Nokia|PlayStation|PLAYSTATION|Nintendo DSi)/i)) ) {
+	if ( window.location.toString().indexOf("ViewMode=Desktop") == -1 ) {
+		window.top.location.href = "https://rom1125.neompro.net";
+		}
+}
+
